d1867a16c893d74edc6f438643b76211952a0bd8
Squatnet <squatn3t@gmail.com>
2017-08-20 18:21:30 +0100

    Create SystemName.gd
    Example of system file.

---
+++ scripts/Locations/SystemName/SystemName.gd
@@ -0,0 +1,4 @@
+extends Node
+var starClass # not currently used
+var neighbours = {1:"SystemName-2",2:"SystemName-3"} # The neighbouring systems
+var bodies = {1:"BodyName-1",2:"BodyName-2",3:"BodyName-3",4:"BodyName-4",5:"BodyName-5"} #The names of the bodies in the system currently max 7
